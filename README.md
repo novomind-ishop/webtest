932e175230d015cc661d5c08babdb929f5e2ef03
Thomas Stock <tstock@novomind.com>
2018-01-11 17:17:54 +0100

    Improved maven details and added simple test
    Task: ISPS-133
    Change-Id: I5428b9f7331fb9084f90383626008d6dd615d456

---
+++ README.md
@@ -0,0 +1,8 @@
+## Webtest
+[![Build Status](https://travis-ci.org/novomind-ishop/webtest.svg?branch=master)](https://travis-ci.org/novomind-ishop/webtest)
+
+### Build / Usage
+```
+mvn clean package
+java -jar target/webtest.jar --help
+```
---
+++ src/test/java/com/novomind/webtest/MainTest.java
@@ -0,0 +1,55 @@
+package com.novomind.webtest;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.junit.contrib.java.lang.system.SystemErrRule;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import com.google.common.base.Splitter;
+
+public class MainTest {
+
+  @Rule
+  public final SystemErrRule systemErrRule = new SystemErrRule().enableLog();
+  @Rule
+  public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+  @Rule
+  public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+  @Test
+  public void testHelp() throws Exception {
+    exit.expectSystemExitWithStatus(1);
+    exit.checkAssertionAfterwards(() -> assertEquals("This is the novomind webtest tool", outLines().get(0)));
+    exit.checkAssertionAfterwards(() -> assertEquals("", errLines().get(0)));
+    main("--help");
+  }
+
+  @Test
+  public void test() throws Exception {
+    exit.expectSystemExitWithStatus(1);
+    exit.checkAssertionAfterwards(() -> assertEquals("This is the novomind webtest tool", outLines().get(0)));
+    exit.checkAssertionAfterwards(() -> assertEquals("", errLines().get(0)));
+    main();
+  }
+
+  private List<String> outLines() {
+    return lines(systemOutRule.getLogWithNormalizedLineSeparator());
+  }
+
+  private List<String> errLines() {
+    return lines(systemErrRule.getLogWithNormalizedLineSeparator());
+  }
+
+  private List<String> lines(String string) {
+    return Splitter.on("\n").splitToList(string);
+  }
+
+  private void main(String... argsv) throws Exception {
+    Main.main(argsv);
+  }
+}
